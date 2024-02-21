b54e920f1f12e6f7bb6203e55ac23ae577af788c
Adarsh <adarshravi011@gmail.com>
2024-02-21 09:31:33 +0530

    cards changes

--- src/components/SuggestedCourses/SuggestedCourses.jsx
+++ src/components/SuggestedCourses/SuggestedCourses.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import digitalMarketing from '../../assets/images/Digital-Marketing.jpg'
+import beauty from '../../assets/images/makeUp.webp'
+import career from '../../assets/images/rocket.jpg'
+import drone from '../../assets/images/drone.jpg'
+
+
 import './SuggestedCourses.css'
 import { MdOutlineDateRange } from "react-icons/md";
 import { GoClock } from "react-icons/go";
@@ -9,16 +14,6 @@ const SuggestedCourses = () => {
     return (
         <div className='mx-14'>
             <h1 className='text-2xl font-extrabold'>Suggested Course in IIT-ITES</h1>
-            {/* <div className='px-6 mt-5'>
-                <div className='w-80'>
-                    <div className='w-72 h-40 course-card relative z-10'>
-                        <img src={digitalMarketing} alt="" />
-                        <div className='bg-white p-3 w-80 drop-shadow-md rounded-md absolute top-0'>
-                            <h4>Digital Marketing workshops</h4>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
             <div className='px-6 mt-5 grid md:grid-cols-3 lg:grid-cols-4'>
                 <div className='flex flex-col items-center w-72'>
                     <div className='course-card h-40 z-10 -mb-2 '>
@@ -40,36 +35,36 @@ const SuggestedCourses = () => {
                 </div>
                 <div className='flex flex-col items-center w-72'>
                     <div className='course-card w-72 h-40 z-10 -mb-2 '>
-                        <img src={digitalMarketing} alt="" />
+                        <img src={beauty} alt="" />
                     </div>
                     <div className='bg-white w-80 p-3 drop-shadow-sm rounded-md'>
-                        <h3 className='font-semibold'>Digital Marketing Workshops</h3>
+                        <h3 className='font-semibold'>Assistant Beauty Therapists</h3>
                         <div className='flex gap-4 text-xs font-light pt-3'>
                             <div className='flex items-center gap-1'>
                                 <p className='text-orange-500 text-lg'><MdOutlineDateRange/></p>
-                                <p className=''>16 Feb 2024</p>
+                                <p className=''>20 Feb 2024</p>
                             </div>
                             <div className='flex items-center gap-1'>
                                 <p className='text-orange-500 text-lg'><GoClock/></p>
-                                <p>2 months</p>
+                                <p>20 days</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='flex flex-col items-center w-72'>
                     <div className='course-card w-72 h-40 z-10 -mb-2 '>
-                        <img src={digitalMarketing} alt="" />
+                        <img src={career} alt="" />
                     </div>
                     <div className='bg-white w-80 p-3 drop-shadow-sm rounded-md'>
-                        <h3 className='font-semibold'>Digital Marketing Workshops</h3>
+                        <h3 className='font-semibold'>Career Development Workshops</h3>
                         <div className='flex gap-4 text-xs font-light pt-3'>
                             <div className='flex items-center gap-1'>
                                 <p className='text-orange-500 text-lg'><MdOutlineDateRange/></p>
-                                <p className=''>16 Feb 2024</p>
+                                <p className=''>05 Feb 2024</p>
                             </div>
                             <div className='flex items-center gap-1'>
                                 <p className='text-orange-500 text-lg'><GoClock/></p>
-                                <p>2 months</p>
+                                <p>2 Days</p>
                             </div>
                         </div>
                     </div>
